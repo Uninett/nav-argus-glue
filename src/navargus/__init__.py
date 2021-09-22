225b5e31d958343c0777f2edaa24a0acd8b3f7d0
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-09-22 11:33:03 +0200

    Bump version to 0.6.1

--- src/navargus/__init__.py
+++ src/navargus/__init__.py
@@ -20,7 +20,7 @@ Exports events from NAV's Event Engine streaming interface into an Argus server.
 
 JSON parsing inspired by https://stackoverflow.com/a/58442063
 """
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 import select
 import sys
