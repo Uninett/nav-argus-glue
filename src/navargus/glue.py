df09a1343b561411624f35ff416426638bf285fd
Johanna England <johanna.england@sikt.no>
2025-02-25 13:01:54 +0100

    Replace sync on startup with sync-interval setting
    This way the user can set how often a re-sync is run
    If it is set to null no re-sync will be run

--- src/navargus/glue.py
+++ src/navargus/glue.py
@@ -31,7 +31,7 @@ import argparse
 import time
 from datetime import datetime, timedelta
 from json import JSONDecoder, JSONDecodeError
-from typing import Generator, Tuple, List
+from typing import Generator, List, Tuple, Optional
 
 import yaml
 
@@ -60,7 +60,6 @@ NOT_WHITESPACE = re.compile(r"[^\s]")
 NAV_SERIES = tuple(int(i) for i in _NAV_VERSION.split(".")[:2])
 NAV_VERSION_WITH_SEVERITY = (5, 2)
 SELECT_TIMEOUT = 30.0  # seconds
-RESYNC_INTERVAL = timedelta(minutes=1)
 
 
 def main():
@@ -77,8 +76,6 @@ def main():
     elif parser.sync:
         do_sync()
     else:
-        if _config.get_sync_on_startup():
-            do_sync()
         read_eventengine_stream()
 
 
@@ -147,9 +144,13 @@ def emit_json_objects_from(stream, buf_size=1024, decoder=JSONDecoder()):
     buffer = ""
     last_block_size = 0
     error = None
-    last_full_sync = None
+    last_full_sync: Optional[datetime] = None
     while True:
-        if not last_full_sync or last_full_sync + RESYNC_INTERVAL < datetime.now():
+        sync_interval = _config.get_sync_interval()
+        if sync_interval and (
+            not last_full_sync
+            or last_full_sync + timedelta(minutes=sync_interval) < datetime.now()
+        ):
             do_sync()
             last_full_sync = datetime.now()
 
@@ -567,9 +568,15 @@ class Configuration(dict):
         """Returns the configured API request timeout value"""
         return float(self.get("api", {}).get("timeout", 2.0))
 
-    def get_sync_on_startup(self):
-        """Returns True if this program should always sync the Argus API on startup"""
-        return bool(self.get("api", {}).get("sync-on-startup"))
+    def get_sync_interval(self):
+        """
+        Returns the interval in minutes of how often the Argus API is synced if
+        it is None, no re-sync should happen
+        """
+        sync_interval = self.get("api", {}).get("sync-interval", 1)
+        if sync_interval is None:
+            return None
+        return int(sync_interval)
 
     def get_default_level(self) -> int:
         return int(self.get("api", {}).get("default-level", 3))
